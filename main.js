9c87f7cc8b83becd75602286a3fc8ef23e9958ee
victoriawisniewskiribeiro-hub <victoria.wisniewski.ribeiro@escola.pr.gov.br>
2026-07-06 12:00:48 -0300

    y

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const botoes = document.querySelectorAll("button")
 console.log(botoes)
+
+botoes.forEach(function (botao){
+   let curtiu = false
+   botao.addEventListener("click",botaoClicado)
+
+   function botaoClicado(){
+     console.log("botao Clicado")
+   }
+})
